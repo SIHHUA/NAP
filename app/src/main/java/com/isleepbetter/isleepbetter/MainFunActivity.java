73f9c40c5fab66dca0d30a98290693fed4c52712
SIHHUA <103316224@gms.tcu.edu.tw>
2020-03-28 18:57:36 +0800

    main function in progress.

--- app/src/main/java/com/isleepbetter/isleepbetter/MainFunActivity.java
+++ app/src/main/java/com/isleepbetter/isleepbetter/MainFunActivity.java
@@ -1,13 +1,7 @@
 package com.isleepbetter.isleepbetter;
 
-import android.os.Bundle;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import android.os.Bundle;
 
 public class MainFunActivity extends AppCompatActivity {
 
@@ -15,15 +9,5 @@ public class MainFunActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_fun);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
     }
-
 }
